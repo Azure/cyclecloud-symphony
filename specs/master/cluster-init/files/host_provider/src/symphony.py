d31d2370202ae14de092a334ffdfa8da31a9bab7
Ben Watrous <ben.watrous@microsoft.com>
2022-01-07 14:19:21 -0800

    revert to using EGOSH to lookup the REST service - in the case of multiple ManagementHosts, the hostname is not static

--- specs/master/cluster-init/files/host_provider/src/symphony.py
+++ specs/master/cluster-init/files/host_provider/src/symphony.py
@@ -38,15 +38,38 @@ class SymphonyRestClient:
         self.token = None
 
     def rest_url(self):
-        # It's possible to the url from ego (supports manual cluster config changes)
-        #     egosh client view REST_HOST_FACTORY_URL | grep DESCRIPTION | sed 's/^DESCRIPTION\:\s//g' -
-        # But HostFactory does not have the correct environment for egosh
-        # Instead, generate the url from cluster config
-        webserviceHostname = self.config.get('symphony.hostfactory.rest_address', '127.0.0.1')
-        webservicePort = self.config.get('HF_REST_LISTEN_PORT', self.config.get('symphony.hostfactory.HF_REST_LISTEN_PORT', '9080'))
-        webserviceSsl = self.config.get('symphony.hostfactory.HF_REST_TRANSPORT', 'TCPIPv4').lower() == 'TCPIPv4SSL'.lower()
-        prefix = 'https' if webserviceSsl else 'http'
-        url = '%s://%s:%s/platform/rest/hostfactory' % (prefix, webserviceHostname, webservicePort)
+        import os
+        import subprocess
+
+        egosh_cmd = "egosh"
+        url = None
+        try:
+            # Firt, try to get the url from ego (supports manual cluster config changes)
+            # egosh client view REST_HOST_FACTORY_URL | grep DESCRIPTION | sed 's/^DESCRIPTION\:\s//g' -
+            # add which or command -v
+            if "EGO_BINDIR" in os.environ:
+                egosh_cmd = os.path.join(os.environ["EGO_BINDIR"], egosh_cmd)
+            elif "EGO_LIBDIR" in os.environ:
+                egosh_cmd = os.path.join(os.environ["EGO_LIBDIR"], "..", "bin", egosh_cmd)
+            else:
+                # which returns nothing, so revert to the default
+                egosh_cmd = which(egosh_cmd) or egosh_cmd
+
+            client_view = subprocess.check_output([egosh_cmd, 'client', 'view', 'REST_HOST_FACTORY_URL']).decode()
+            description = [x for x in client_view.splitlines() if 'DESCRIPTION' in x][0]
+            url = description.split()[1]
+        except:        
+            self.logger.exception(f"Failed to read REST service URL from {egosh_cmd}.  Will attempt to use local config.")
+
+            # It's possible to the url from ego (supports manual cluster config changes)
+            #     egosh client view REST_HOST_FACTORY_URL | grep DESCRIPTION | sed 's/^DESCRIPTION\:\s//g' -
+            # But HostFactory does not have the correct environment for egosh
+            # Instead, generate the url from cluster config
+            webserviceHostname = self.config.get('symphony.hostfactory.rest_address', '127.0.0.1')
+            webservicePort = self.config.get('HF_REST_LISTEN_PORT', self.config.get('symphony.hostfactory.HF_REST_LISTEN_PORT', '9080'))
+            webserviceSsl = self.config.get('HF_REST_TRANSPORT', self.config.get('symphony.hostfactory.HF_REST_TRANSPORT', 'TCPIPv4')).lower() == 'TCPIPv4SSL'.lower()
+            prefix = 'https' if webserviceSsl else 'http'
+            url = '%s://%s:%s/platform/rest/hostfactory' % (prefix, webserviceHostname, webservicePort)
 
         self.logger.info(f"Using HF URL: {url.rstrip('/')}")
         return url.rstrip('/')
@@ -63,7 +86,7 @@ class SymphonyRestClient:
     
     def _login(self):
         url = self.webserviceUrl + '/auth/login'
-        r = requests.get(url, auth=(self.username, self.password))
+        r = requests.get(url, auth=(self.username, self.password), verify=False)
         self._raise_on_error(r)
         
         hfcsrftokenBody = r.json()
@@ -74,5 +97,5 @@ class SymphonyRestClient:
         hfcsrftoken = self._login()
         params = {'hfcsrftoken': hfcsrftoken}
         url = self.webserviceUrl + '/provider/azurecc/templates'
-        r = requests.put(url, auth=(self.username, self.password), params=params, json=templates)
+        r = requests.put(url, auth=(self.username, self.password), params=params, json=templates, verify=False)
         self._raise_on_error(r)
