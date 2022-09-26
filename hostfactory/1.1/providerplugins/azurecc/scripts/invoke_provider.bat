ced0a2efa3b4e9c677c7899cfba54853a2dac69f
nidhi0622 <nidhimehta@microsoft.com>
2022-09-26 10:02:31 -0500

    corrected the if condition

--- hostfactory/1.1/providerplugins/azurecc/scripts/invoke_provider.bat
+++ hostfactory/1.1/providerplugins/azurecc/scripts/invoke_provider.bat
@@ -3,8 +3,12 @@ set PRO_CONF_DIR=%HF_CONFDIR%\providers\azurecc
 set PRO_DATA_DIR=%HF_WORKDIR%
 set STDERR_FILE=%HF_LOGDIR%\azurecc_invoke.err
 set dirname=%~dp0
-if exist %dirname%\..\.venv\ (call %dirname%\..\.venv\azurecc\Scripts\activate.bat
-set embedded_python="%dirname%\..\.venv\azurecc\Scripts\python.exe" ) else (set embedded_python=C:\cycle\jetpack\system\embedded\python\python.exe)
+if exist "%dirname%\..\.venv\" (
+    call %dirname%\..\.venv\azurecc\Scripts\activate.bat
+    set embedded_python="%dirname%\..\.venv\azurecc\Scripts\python.exe" 
+) else (
+    set embedded_python=C:\cycle\jetpack\system\embedded\python\python.exe
+)
 set PYTHONPATH=%PYTHONPATH%;%dirname%\src
 SET > %TEMP%\invoke.log
 echo >>  %TEMP%\invoke.log
