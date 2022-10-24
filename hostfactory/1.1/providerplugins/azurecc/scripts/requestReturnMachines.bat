301cb3a9a3e9f8de858da91ddaa17388256a5c5d
nidhi0622 <nidhimehta@microsoft.com>
2022-10-24 12:40:45 -0500

    using scalelib for add nodes, status,deallocate

--- hostfactory/1.1/providerplugins/azurecc/scripts/requestReturnMachines.bat
+++ hostfactory/1.1/providerplugins/azurecc/scripts/requestReturnMachines.bat
@@ -1,7 +1,8 @@
+@echo off
 ::xcopy /S /Q /Y /F %2 %TEMP%\reqRetMach.input.json
 :: copy second argument to tmp folder
 set dirname=%~dp0
-"%dirname%invoke_provider.bat" terminate_machines %*
+cmd /c "%dirname%invoke_provider.bat" terminate_machines %*
 
 set ret=%errorlevel%
 exit /b %ret%
