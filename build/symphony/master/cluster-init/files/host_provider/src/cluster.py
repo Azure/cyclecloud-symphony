b405523bc3f0984cae35a89d97d24d33a5c8fbdd
nidhi0622 <nidhimehta@microsoft.com>
2023-03-28 14:23:07 -0500

    updated review comments

---
+++ build/symphony/master/cluster-init/files/host_provider/src/cluster.py
@@ -0,0 +1,109 @@
+import json
+
+import logging
+from urllib.parse import urlencode
+from builtins import str
+
+
+try:
+    import cyclecli
+except ImportError:
+    import cyclecliwrapper as cyclecli
+    
+
+class Cluster:
+    
+    def __init__(self, cluster_name, provider_config, logger=None):
+        self.cluster_name = cluster_name
+        self.provider_config = provider_config
+        self.logger = logger or logging.getLogger()
+    
+    def status(self):
+        return self.get("/clusters/%s/status" % self.cluster_name)
+
+    def add_nodes(self, request):
+        response_raw = self.post("/clusters/%s/nodes/create" % self.cluster_name, json=request)
+        try:
+            return json.loads(response_raw)
+        except:
+            raise RuntimeError("Could not parse response as json to create_nodes! '%s'" % response_raw)
+    
+    def all_nodes(self):
+        return self.get("/clusters/%s/nodes" % self.cluster_name)
+
+    def nodes(self, request_ids):
+        responses = {}
+        for request_id in request_ids:
+            responses[request_id] = self.get("/clusters/%s/nodes" % self.cluster_name, request_id=request_id)
+        return responses
+    
+    def nodes_by_operation_id(self, operation_id):
+        if not operation_id:
+            raise RuntimeError("You must specify operation id!")
+        return self.get("/clusters/%s/nodes?operation=%s" % (self.cluster_name, operation_id))
+
+    def terminate(self, machines):
+        machine_ids = [machine["machineId"] for machine in machines]
+        response_raw = self.post("/clusters/%s/nodes/terminate" % self.cluster_name, json={"ids": machine_ids})
+        try:
+            self.logger.info("Terminate Response: %s", response_raw)
+            return json.loads(response_raw)
+        except:
+            raise RuntimeError("Could not parse response as json to terminate! '%s'" % response_raw)
+
+        # It appears that the "instance-filter" version NEVER succeeds and often throws
+        # kludge: work around
+        # if Symphony may have a stale machineId -> hostname mapping, so find the existing instance with that hostname and kill it
+        # machine_names = [ machine["name"].split(".")[0] for machine in machines if machine.get("name") ]
+        # if machine_names:
+        #     self.logger.warning("Terminating the following nodes by machine_names: %s", machine_names)
+
+        #     f = urlencode({"instance-filter": 'HostName in {%s}' % ",".join('"%s"' % x for x in machine_names)})
+        #     try:
+        #         self.post("/cloud/actions/terminate_node/%s?%s" % (self.cluster_name, f))
+        #     except Exception as e:
+        #         if "No instances were found matching your query" in str(e):
+        #            return
+        #         raise
+            
+    def _session(self):
+        config = {"verify_certificates": False,
+                  "username": self._get_or_raise("cyclecloud.config.username"),
+                  "password": self._get_or_raise("cyclecloud.config.password"),
+                  "cycleserver": {
+                      "timeout": 60
+                  }
+        }
+        return cyclecli.get_session(config=config)
+    
+    def _get_or_raise(self, key):
+        value = self.provider_config.get(key)
+        if not value:
+            #  jetpack.config.get will raise a ConfigError above.
+            raise cyclecli.ConfigError("Please define key %s in the provider config." % key)
+        return value
+    
+    def post(self, url, data=None, json=None, **kwargs):
+        root_url = self._get_or_raise("cyclecloud.config.web_server")
+        self.logger.debug("POST %s with data %s json %s kwargs %s", root_url + url, data, json, kwargs)
+        session = self._session()
+        response = session.post(root_url + url, data, json, **kwargs)
+        response_content = response.content
+        if response_content is not None and isinstance(response_content, bytes):
+            response_content = response_content.decode()
+        if response.status_code < 200 or response.status_code > 299:
+            raise ValueError(response_content)
+        return response_content
+        
+    def get(self, url, **params):
+        root_url = self._get_or_raise("cyclecloud.config.web_server")
+        self.logger.debug("GET %s with params %s", root_url + url, params)
+        session = self._session()
+        response = session.get(root_url + url, params=params)
+        response_content = response.content
+        if response_content is not None and isinstance(response_content, bytes):
+            response_content = response_content.decode()
+
+        if response.status_code < 200 or response.status_code > 299:
+            raise ValueError(response_content)
+        return json.loads(response_content)
---
+++ build/symphony/master/cluster-init/scripts/010_install_symphony_master.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# see: https://www.ibm.com/support/knowledgecenter/SSZUMP_7.2.0/install/install_linux_management.html
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+set -x
+
+. /etc/profile.d/symphony.sh
+
+SOAM_USER=$( jetpack config symphony.soam.user )
+SOAM_PASSWORD=$( jetpack config symphony.soam.password )
+SYM_ENTITLEMENT_FILE=$( jetpack config symphony.license_file )
+SYM_ENTITLEMENT_FILE="/etc/${SYM_ENTITLEMENT_FILE}"
+
+# mgmt nodes don't seem to accept the `EGO_GET_CONF=LIM` setting needed by execs
+sed -i '/EGO_GET_CONF/d' /etc/ego.conf
+
+# mgmt nodes may already be "joined" (and that may cause a non-zero exit)
+set +e
+su - -c 'source /etc/profile.d/symphony.sh && yes | egoconfig join $( hostname ) | tee -a /tmp/egojoin' egoadmin
+if [ $? -ne 0 ]; then
+    if grep -q "This host belongs to the cluster ${CLUSTERNAME}" /tmp/egojoin; then
+        echo "Host already configured for this cluster (${CLUSTERNAME})"
+    else
+        exit -1
+    fi
+fi
+
+set -e
+
+
+su - -c "source /etc/profile.d/symphony.sh && yes | egoconfig setentitlement ${SYM_ENTITLEMENT_FILE}" egoadmin
+
+# Enable automatic startup after reboot (TODO: might want to move restart to chef so volumes are mounted)
+egosetrc.sh
+
+# Grant sudoer access to egosudoers in file /etc/ego.sudoers (by default: egoadmin and root)
+egosetsudoers.sh -f
+su - -c 'source /etc/profile.d/symphony.sh && yes | egosh ego start' egoadmin
+
+# Give the services 10 tries to start
+counter=0
+until su - -c "source /etc/profile.d/symphony.sh && egosh user logon -u ${SOAM_USER} -x ${SOAM_PASSWORD}" egoadmin; do
+    if [[ "$counter" -gt 10 ]]; then
+	echo "Failed to connect to cluster after $counter retries.  Aborting..."
+	exit -1
+    else
+	counter=$((counter+1))
+	sleep 5
+	echo "Retry $counter..."
+    fi
+done
+
+# Verify setup
+su - -c 'source /etc/profile.d/symphony.sh && egosh ego info' egoadmin
+su - -c 'source /etc/profile.d/symphony.sh && egosh client view' egoadmin
+
