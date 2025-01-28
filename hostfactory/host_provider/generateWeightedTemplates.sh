f6269b2ff64d480268f3e19bf5a26ca0b65fc21b
Ben Watrous <ben.watrous@microsoft.com>
2025-01-28 15:26:06 -0800

    ensure that calling user has permission to write the input json file

--- hostfactory/host_provider/generateWeightedTemplates.sh
+++ hostfactory/host_provider/generateWeightedTemplates.sh
@@ -6,20 +6,18 @@ TEMP_HF_WORKDIR=/tmp/work
 export HF_WORKDIR=$TEMP_HF_WORKDIR
 mkdir -p $HF_LOGDIR
 mkdir -p $HF_WORKDIR
-cat <<EOF >/tmp/genTemplates.input.json
+cat <<EOF >/tmp/genTemplates.input.${USER}.json
 {}
 EOF
 export PRO_LOG_DIR=${HF_LOGDIR}
 export PRO_CONF_DIR=${HF_CONFDIR}/providers/azurecc
 export PRO_DATA_DIR=${HF_WORKDIR}
 
-env > /tmp/invoke2.env
-
 venv_path=$HF_TOP/$HF_VERSION/providerplugins/azurecc/venv/bin
 scriptDir=`dirname $0`
 export PYTHONPATH=$PYTHONPATH:$scriptDir/src
 . $venv_path/activate
-$venv_path/python3 -m cyclecloud_provider generate_templates -f /tmp/genTemplates.input.json
+$venv_path/python3 -m cyclecloud_provider generate_templates -f /tmp/genTemplates.input.${USER}.json
 exit_status=$?
 rm -rf $TEMP_HF_LOGDIR
 rm -rf $TEMP_HF_WORKDIR
